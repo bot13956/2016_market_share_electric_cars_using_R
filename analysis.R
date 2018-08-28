cffe2debb42ad70fe92f009b3e08193ee7a01909
Benjamin Obi Tayo <benjaminobi@gmail.com>
2018-08-28 16:45:06 -0500

    Add files via upload

---
+++ analysis.R
@@ -0,0 +1,10 @@
+data<-read_csv("global_EV_2016.csv",col_names = FALSE)
+head(data)
+data<-data[-c(1,2),]
+names(data)<-c("country","sales_bev","stock_bev","sales_phev","stock_phev","shares")
+head(data)
+
+data%>%drop_na(shares)%>%mutate(shares=parse_number(shares))%>%
+  filter(shares>=0.91)%>%ggplot(aes(reorder(country, shares),shares))+geom_col(fill="blue")+
+  coord_flip()+ theme(axis.title.x=element_blank())+theme(axis.title.y=element_blank())+
+  ggtitle("Market share of electric vehicles in selected countries (%)")
